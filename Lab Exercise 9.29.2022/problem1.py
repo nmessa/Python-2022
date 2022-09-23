6e1c25eef4447697cb4c7ee6d85327d9505f0902
nmessa <nmessa@sau16.org>
2022-09-23 11:43:35 -0400

    Add files via upload

---
+++ Lab Exercise 9.29.2022/problem1.py
@@ -0,0 +1,15 @@
+## Lab Exercise 9/29/2022 Problem 1
+## Author: 
+## This program draws a blue rectangle with my name in it
+
+from graphics import *
+
+# Create the window and access the canvas.
+#Add code here
+
+# Draw on the canvas.
+#Add code here
+
+
+# Wait for the user to close the window.
+#Add code here
