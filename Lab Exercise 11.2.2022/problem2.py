5e09562b78295c8395bed47088ef8305ecb80972
nmessa <nmessa@sau16.org>
2022-10-28 07:28:35 -0400

    Add files via upload

---
+++ Lab Exercise 11.2.2022/problem2.py
@@ -0,0 +1,11 @@
+##Lab Exercise 11/2/2022 Problem 2
+##Author: 
+##This program will calculate a number raised to a power
+
+def power(base, exponent):
+    #Add code here
+    
+
+print (power(2, 5)) #32
+
+    
---
+++ Lab Exercise 11.3.2022/dna1_starter.py
@@ -0,0 +1,13 @@
+## Lab Exercise 11/3/2022 Problem 1
+## Author: 
+## Filename: dna1_starter.py
+## This program demonstrates DNA sequence matching
+
+#create test sequence
+strand = "AACAACTTCGTAAGTAA"
+look = "TCGT"
+
+
+#search sequence
+#ADD CODE HERE
+
