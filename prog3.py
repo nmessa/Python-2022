84e5f3cc705d8987fc0a1189a9b1b1e832a3ce01
nmessa <nmessa@sau16.org>
2022-11-18 14:45:53 -0500

    Delete prog3.py

--- prog3.py
+++
@@ -1,9 +0,0 @@
-import machine
-import utime
-
-button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-
-while True:
-    if button.value() == 1:
-        print("You pressed the button!")
-        utime.sleep(2)
