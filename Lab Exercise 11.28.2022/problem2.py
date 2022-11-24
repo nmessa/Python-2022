28fd6c7a2a5accf164e4e345b6c9c1b283ecfd49
nmessa <nmessa@sau16.org>
2022-11-24 09:28:37 -0500

    Add files via upload

---
+++ Lab Exercise 11.28.2022/problem2.py
@@ -0,0 +1,14 @@
+## Lab Exercise 11/28/2022 Problem 2
+## Author: 
+## Function that returns a boolean value if word is a palindrome
+
+
+
+def isPalindrome(word):
+    #Add code here
+    
+    
+print (isPalindrome('KAYAK'))     #True
+print (isPalindrome('Hello'))     #False
+print (isPalindrome('abccba'))    #True
+        
---
+++ Lab Exercise 11.29.2022/problem3.py
@@ -0,0 +1,10 @@
+## Lab Exercise 11/29/2022 Problem 3
+## Author: 
+## Print ASCII table
+
+def printASCII():
+    #Add code here
+    
+
+#Test code
+printASCII()  
