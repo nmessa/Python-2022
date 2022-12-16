9a86dbc43710b413ca0ff19fd4832e080fdf2015
nmessa <nmessa@sau16.org>
2022-12-16 12:16:45 -0500

    Add files via upload

---
+++ Lab Exercise 12.21.2022/problem 1b.py
@@ -0,0 +1,11 @@
+## Lab Exercise 12/21/2022 Problem 1b
+## Author: nmessa
+## Draws rectangles using alternative graphics module
+
+from graphics import *
+
+def main():
+    #Add code here
+    
+
+main()
---
+++ Lab Exercise 12.21.2022/problem 2.py
@@ -0,0 +1,12 @@
+## Lab Exercise 12/21/2022 Problem 2
+## Author: 
+## Draw an archery target
+
+from graphics import *
+import time
+
+def main():
+    #Add code here
+    
+
+main()
