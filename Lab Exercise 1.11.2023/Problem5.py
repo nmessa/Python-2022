a969376a30f83d57de37ca90f8150ace72b14b7f
nmessa <nmessa@sau16.org>
2023-01-06 14:11:38 -0500

    Add files via upload

---
+++ Lab Exercise 1.11.2023/Problem5.py
@@ -0,0 +1,17 @@
+##Lab Exercise 1.11.2023 Problem 1
+##Author: nmessa
+
+def free_throws(percent, num):
+    #Add code here
+
+    
+        
+#Test code
+print(free_throws("75%", 5))
+print(free_throws("25%", 3))
+print(free_throws("90%", 30))
+
+##Output
+##24%
+##2%
+##4%
